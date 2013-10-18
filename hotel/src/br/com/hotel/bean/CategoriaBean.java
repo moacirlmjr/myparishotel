0af1b0ccc22f25a682158cef2eb1907a8a24c3d9
danyllowagneralbuquerque <danyllowagneralbuquerque@8a0279ec-437f-3051-06e8-21faaa35a264>
2013-10-18 23:41:37 +0000

    SOlucao CAtegoriaQuarto

--- hotel/src/br/com/hotel/bean/CategoriaBean.java
+++ hotel/src/br/com/hotel/bean/CategoriaBean.java
@@ -1,5 +1,8 @@
 package br.com.hotel.bean;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
@@ -10,39 +13,40 @@ import br.com.hotel.util.JSFMessageUtil;
 @ManagedBean
 @ViewScoped
 public class CategoriaBean {
-	
-	private Categoria categoria = new Categoria();
-	
-	public CategoriaBean() {
-		categoria= new Categoria();
-	}
 
-	
+	private Categoria categoria = new Categoria();
+
+	public CategoriaBean() {
+		categoria = new Categoria();
+	}
 
 	public Categoria getCategoria() {
 		return categoria;
 	}
 
+	public List<Categoria> getCategorias() {
+		ArrayList<Categoria> categorias = (ArrayList<Categoria>) new DAO<Categoria>(
+				Categoria.class).listaTodos();
 
+		return categorias;
+	}
 
 	public void setCategoria(Categoria categoria) {
 		this.categoria = categoria;
 	}
 
-
-
 	public String gravar() {
-		System.out.println("Gravando categoria " + this.categoria.getCategoria());
-		try{
+		System.out.println("Gravando categoria "
+				+ this.categoria.getCategoria());
+		try {
 			new DAO<Categoria>(Categoria.class).adiciona(this.categoria);
 			this.categoria = new Categoria();
-		}catch(Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		JSFMessageUtil.sendInfoMessageToUser("Categoria Inserida com sucesso!!!");
+		JSFMessageUtil
+				.sendInfoMessageToUser("Categoria Inserida com sucesso!!!");
 		return "categoriaQuarto?faces-redirect=true";
 	}
-
-	
 
 }
